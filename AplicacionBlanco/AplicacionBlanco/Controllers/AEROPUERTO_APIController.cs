506a748052ba85fdf066c0e6d8ee387163a3f89c
lmonsalve <lmonsalve22@gmail.com>
2020-06-11 12:26:50 -0400

    commit

---
+++ AplicacionBlanco/AplicacionBlanco/Controllers/AEROPUERTO_APIController.cs
@@ -0,0 +1,65 @@
+﻿using AplicacionBlanco.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionBlanco.Controllers
+{
+    public class AEROPUERTO_APIController : Controller
+    {
+        // GET: AEROPUERTO_API
+        public JsonResult Get()
+        {
+            
+            //var subtemas = db.SUBTEMAS.Where(x => x.TEMA_id == id);
+            var salida = new AEROPUERTOS();
+            //return Json(salida.Aeropuertos, JsonRequestBehavior.AllowGet);
+            //return Json(salida.Aeropuertos.GetRange(1, 5000), JsonRequestBehavior.AllowGet);
+            return Json(salida.Aeropuertos[1], JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Buscador(string id)
+        {
+            id = id.ToUpper();
+            var salida = new AEROPUERTOS();
+            if (id.Length <= 2)
+            {
+                return Json(new List<AEROPUERTO>(), JsonRequestBehavior.AllowGet);
+            }
+            if (id.Length > 2 && id.Length < 5)
+            {
+                return Json(salida.Aeropuertos.Where(m => m.IATA.Contains(id) || m.ICAO.Contains(id)), JsonRequestBehavior.AllowGet);
+            }            
+            return Json(salida.Aeropuertos.Where(m => m.NOMBRE.ToUpper().Contains(id)), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Test(int id,int id2)
+        {
+            var salida = new AEROPUERTOS();
+            AEROPUERTO p1 = salida.Aeropuertos.Where(m => m.AIRPORT_ID == id).First();
+            AEROPUERTO p2 = salida.Aeropuertos.Where(m => m.AIRPORT_ID == id2).First();
+            /*
+            p1.LATITUD = 1;
+            p2.LATITUD = 0;
+            p1.LONGITUD = 0;
+            p2.LONGITUD = 0;
+            */
+            //int salida = 1;
+            return Json(CALCULOS.ValorFinal(p1,p2), JsonRequestBehavior.AllowGet);
+        }
+        /*
+        public string Get(string id)
+        {
+            //var subtemas = db.SUBTEMAS.Where(x => x.TEMA_id == id);
+            var salida = new AEROPUERTOS();
+            //return Json(salida.Aeropuertos, JsonRequestBehavior.AllowGet);
+            //return Json(salida.Aeropuertos.Where(m => m.NOMBRE.Contains(id)), JsonRequestBehavior.AllowGet);
+            //return Json(salida.Aeropuertos[1], JsonRequestBehavior.AllowGet);
+            return "Hola";
+
+        }
+        */
+    }
+}
---
+++ AplicacionBlanco/AplicacionBlanco/Models/CALCULOS.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionBlanco.Models
+{
+    public class CALCULOS
+    {
+        static AEROPUERTOS Aeropuertos = new AEROPUERTOS();
+
+        public static double CalcularDistancia(AEROPUERTO p1, AEROPUERTO p2)
+        {
+            double salida = 0;
+            double rad_per_grad = Math.PI / 180;
+            double rkm = 6371;
+
+            double dLatitud_rad = (p2.LATITUD - p1.LATITUD) * rad_per_grad;
+            double dLongitud_rad = (p2.LONGITUD - p1.LONGITUD) * rad_per_grad;
+
+            double latitud1_rad = p1.LATITUD * rad_per_grad;
+            double latitud2_rad = p2.LATITUD * rad_per_grad;
+
+            double sinDLatitud = Math.Sin(dLatitud_rad / 2);
+            double sinDLongitud = Math.Sin(dLongitud_rad / 2);
+
+            double a = sinDLatitud * sinDLatitud + 
+                       Math.Cos(latitud1_rad) * Math.Cos(latitud2_rad) *
+                       sinDLongitud * sinDLongitud;
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return rkm * c;
+        }
+
+        public static double CalcularCO2(double distancia)
+        {
+            return distancia * 0.32 / 1000;
+        }
+
+        public static int CalcularValor(double CO2)
+        {
+            return Convert.ToInt32(CO2 * 80);
+        }
+
+        public static int ValorFinal(AEROPUERTO p1, AEROPUERTO p2)
+        {
+            return CalcularValor(CalcularCO2(CalcularDistancia(p1, p2)));
+        }
+    }
+}
